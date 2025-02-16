a6061f6187fd4347e21756ba1af768c82f6b032f
angelcaz8 <angeljcaza1998@gmail.com>
2025-02-16 13:40:16 -0600

    ejercicio de libros leidos

---
+++ funciones.js
@@ -0,0 +1,14 @@
+let librosLeidos = ["EL PERFUME"]; // Array para almacenar los libros
+
+// Función para agregar libros
+function agregarLibro(titulo) {
+ librosLeidos.push(titulo);
+}
+
+// Función para mostrar libros (usando for clásico)
+function mostrarLibrosLeidos() {
+     for (let i = 0; i < librosLeidos.length; i++) {
+ console.log(librosLeidos[i]);
+ }
+}
+mostrarLibrosLeidos();
